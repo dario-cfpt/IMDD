55f167f51d5d91675e16976541bd55b16561b40e
Dylan Peiry <dylan.pr@eduge.ch>
2018-03-09 10:26:32 +0100

    Création du fichier php pour gérer les pages

---
+++ IMDD/DF/imdd.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Author : Dylan Peiry
+ * Date : 09.03.2018
+ * Name : IMDD
+ * Description : A definir
+ * Version : 1.0
+ */
+
+if (empty($_GET)) {
+    echo '<h1 style="color: rgba(203,0,78,0.74);">Aucun paramètre reçu</h1>';
+} else {
+
+}
