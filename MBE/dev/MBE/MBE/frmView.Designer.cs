8a3c4ba7d80ade4e25980f82a74e0a437b79e749
dario-cfpt <34121538+dario-cfpt@users.noreply.github.com>
2018-03-16 11:44:42 +0100

    Début des méthodes d'affichages pour la form frmView

--- MBE/dev/MBE/MBE/frmView.Designer.cs
+++ MBE/dev/MBE/MBE/frmView.Designer.cs
@@ -33,7 +33,9 @@
             this.aideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.grpbxEmulator = new System.Windows.Forms.GroupBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.msMain.SuspendLayout();
+            this.grpbxEmulator.SuspendLayout();
             this.SuspendLayout();
             // 
             // msMain
@@ -54,24 +56,28 @@
             this.configurationToolStripMenuItem.Name = "configurationToolStripMenuItem";
             this.configurationToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
             this.configurationToolStripMenuItem.Text = "Configuration";
+            this.configurationToolStripMenuItem.Click += new System.EventHandler(this.configurationToolStripMenuItem_Click);
             // 
             // aideToolStripMenuItem
             // 
             this.aideToolStripMenuItem.Name = "aideToolStripMenuItem";
             this.aideToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
             this.aideToolStripMenuItem.Text = "Aide";
+            this.aideToolStripMenuItem.Click += new System.EventHandler(this.aideToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
             this.toolStripMenuItem1.Text = "?";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // grpbxEmulator
             // 
             this.grpbxEmulator.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpbxEmulator.Controls.Add(this.webBrowser1);
             this.grpbxEmulator.Location = new System.Drawing.Point(12, 43);
             this.grpbxEmulator.Name = "grpbxEmulator";
             this.grpbxEmulator.Size = new System.Drawing.Size(569, 352);
@@ -79,17 +85,27 @@
             this.grpbxEmulator.TabStop = false;
             this.grpbxEmulator.Text = "Écrans fictifs";
             // 
-            // frmEmulator
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(6, 19);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(537, 311);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://10.134.97.174/snake.html", System.UriKind.Absolute);
+            // 
+            // frmView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(593, 407);
             this.Controls.Add(this.grpbxEmulator);
             this.Controls.Add(this.msMain);
-            this.Name = "frmEmulator";
+            this.Name = "frmView";
             this.Text = "Multiple Browser Emulator";
             this.msMain.ResumeLayout(false);
             this.msMain.PerformLayout();
+            this.grpbxEmulator.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,5 +118,6 @@
         private System.Windows.Forms.ToolStripMenuItem aideToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.GroupBox grpbxEmulator;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
